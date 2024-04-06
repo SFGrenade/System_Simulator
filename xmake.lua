9cef7b956efcef7b44e5c2fe3ad19ebc0339baa1
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-04-06 12:43:34 +0200

    set c++11 more compile modes remove unneeded xmake.lua things

--- xmake.lua
+++ xmake.lua
@@ -1,14 +1,13 @@
-add_rules( "mode.debug", "mode.release" )
-add_rules( "plugin.compile_commands.autoupdate", { outputdir = ".vscode" } )
-
 set_project( "System_Simulator" )
 
+set_version( "0.0.1", { build = "%Y%m%d", soname = true } )
+
+add_rules( "mode.debug", "mode.release", "mode.releasedbg", "mode.minsizerel" )
+add_rules( "plugin.compile_commands.autoupdate", { outputdir = ".vscode" } )
+
+set_languages( "c++11" )
+
 if is_plat( "windows" ) then
-    set_languages( "cxxlatest" )
-
-    -- bruh
-    --add_defines( "_WIN32_WINNT=0x0601" )
-
     add_cxflags( "/Zc:__cplusplus" )
     add_cxflags( "/Zc:preprocessor" )
 
@@ -18,23 +17,12 @@ if is_plat( "windows" ) then
     -- MDd somehow breaks installation of packages lol
     --set_runtimes(is_mode("debug") and "MDd" or "MD")
     set_runtimes(is_mode("debug") and "MD" or "MD")
-else
-    set_languages( "cxx20" )
 end
 
 set_warnings( "allextra" )
 
-if is_mode( "debug" ) then
-    add_defines( "DEBUG" )
-end
-if is_mode( "release" ) then
-    add_defines( "NDEBUG" )
-end
-
 -- maybe this helps for the ci?
 set_policy("build.across_targets_in_parallel", false)
-
---add_repositories( "local-repo E:\\xmake_packages" )
 
 add_requireconfs( "*", { configs = { shared = get_config( "kind" ) == "shared" } } )
 
